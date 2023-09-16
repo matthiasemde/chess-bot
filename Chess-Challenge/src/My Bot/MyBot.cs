f3e5c9f8fb8679355033c7486add3f6b3084981a
Matthias Emde <matthias@emdemail.de>
2023-09-16 17:50:47 +0200

    modified weight encoder and added move eval to nn

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,42 +1,109 @@
-﻿// #define DEBUG
+﻿#define DEBUG_LEVEL_1
+// #define DEBUG_LEVEL_2
 
 using System;
 using System.Diagnostics;
+using System.Linq;
 using ChessChallenge.API;
+using Microsoft.CodeAnalysis;
 
 public class MyBot : IChessBot
 {
     // Encoded weights
-    decimal[] weights = {
-        39730440962195658276260069600m,39730440962195658276260069600m,39730440962195658276260069600m,39730440962195658276260069600m,
-        39730440962195658276260069600m,39730440962195658276260069600m,39730440962195658276260069600m,39730440962195658276260069600m,
-        39730440962195658276260069600m,39730440962195658276260069600m,39730440962195658276260069600m,39730440962195658276260069600m,
-        39730440962195658276260069600m,
-    }; 
+    readonly decimal[] weights = {
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+        8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,8200574590949850612662389230m,
+    };
+    readonly double scaling_factor = 73.143;
+    readonly double shift = -0.25;
 
     public unsafe MyBot()
     {
 
-        // create neurons for a (12, 12, 1) network
-        int[] layers = {12, 12, 1}; // optimize later
-        double[] neurons = new double[12 + 12 + 1]; // optimize later
+    }
 
-        #if DEBUG
-        Debug.Assert(weights.Length * 12 == (layers[0] * layers[1] + layers[1] * layers[2]), "Number of weights needs to match network connections!");
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] moves = board.GetLegalMoves();
+
+        double[] evaluations = new double[moves.Length];
+
+        #if DEBUG_LEVEL_1
+            Console.WriteLine("\n-----------------------\nNext move\n");
         #endif
 
-        // copy board state to neurons
-        for (int i = 0; i < 12; i++)
+        // evaluate each move
+        for (int i = 0; i < moves.Length; i++)
         {
-            neurons[i] = 1.0;
+            // make move
+            board.MakeMove(moves[i]);
+
+            // evaluate board
+            evaluations[i] = Evaluate(board);
+
+            #if DEBUG_LEVEL_1
+                Console.WriteLine("Move: " + moves[i].ToString() + " Eval: " + evaluations[i]);
+            #endif
+
+            // undo move
+            board.UndoMove(moves[i]);
         }
 
-        // compute feed forward
+        // find best move
+        return moves[0];
+    }
+
+    private unsafe double Evaluate(Board board)
+    {
+        // create neurons for a (12, 12, 1) network
+        int[] layers = {768, 1}; // optimize later
+        double[] neurons = new double[768 + 1]; // optimize later
+
+        #if DEBUG
+        int networkConnections = 0;
+        for(int i = 1; i < layers.Length; i++)
+        {
+            networkConnections += layers[i-1] * layers[i];
+        }
+        Debug.Assert(weights.Length * 12 == networkConnections, "Number of weights (" + weights.Length * 12 + ") needs to match network connections (" + networkConnections +")!");
+        #endif
+
         fixed (double * neuron_po = neurons)
         {
+            double * input_p = neuron_po;
+
+            // copy board state to neurons
+            #if DEBUG_LEVEL_2
+                Console.WriteLine("\nBoard:\n");
+            #endif
+            foreach (PieceList pieceList in board.GetAllPieceLists())
+            {
+                #if DEBUG_LEVEL_2
+                    Console.WriteLine("\nPiece: " + pieceList.TypeOfPieceInList + " (" + (pieceList.IsWhitePieceList ? "white" : "black") +")\n");
+                #endif
+                for (int i = 0; i < 64; i++) {
+                    *input_p++ = board.GetPieceBitboard(pieceList.TypeOfPieceInList, pieceList.IsWhitePieceList) >> i & 1;
+                    #if DEBUG_LEVEL_2
+                        Console.Write(*(input_p - 1) + " ");
+                        if (i % 8 == 7) Console.WriteLine();
+                    #endif
+                }
+            }
+
+            #if DEBUG_LEVEL_2
+                Console.WriteLine("\n\nNeurons:\n");
+            #endif
+
+            // compute feed forward
             fixed(decimal * weight_po = weights)
             {
-                double * input_p = neuron_po, output_p = neuron_po + 12;
+                double * output_p = neuron_po + layers[0];
                 sbyte * weight_p = (sbyte *) weight_po;
 
                 // loop trough layers
@@ -45,6 +112,9 @@ public class MyBot : IChessBot
                     // loop trough neurons
                     for(int n = 0; n < layers[l]; n++)
                     {
+                        // reset input pointer to beginning of layer
+                        input_p -= layers[l - 1];
+
                         // loop trough inputs
                         for(int i = 0; i < layers[l - 1]; i++)
                         {
@@ -52,14 +122,14 @@ public class MyBot : IChessBot
                             if ((weight_p - (sbyte*) weight_po) % 16 == 0) weight_p += 4;
 
                             // compute weighted sum
-                            *output_p += *input_p * ((double) *weight_p) / 64; // decode sbyte [-128, 127] to double [-2.0, 2.0]
+                            *output_p += *input_p * (((double) *weight_p) / scaling_factor + shift); // decode sbyte [-128, 127] to double [-2.0, 2.0]
 
-                            #if DEBUG
-                            Console.WriteLine(
-                                (*input_p).ToString().PadRight(4) + "("+(input_p - neuron_po)+") * " +
-                                (((double) *weight_p) / 64).ToString().PadRight(4) + "(" + (weight_p - (sbyte*) weight_po) + ") -> " +
-                                (*output_p).ToString().PadRight(4) + "(" + (output_p - neuron_po) + ")"
-                            );
+                            #if DEBUG_LEVEL_2
+                                Console.WriteLine(
+                                    Math.Round(*input_p, 2).ToString().PadRight(8) + "("+ (input_p - neuron_po) +") * " +
+                                    Math.Round(((double) *weight_p) / scaling_factor + shift, 2).ToString().PadRight(8) + "(" + (weight_p - (sbyte*) weight_po) + ") -> " +
+                                    Math.Round(*output_p, 2).ToString().PadRight(8) + "(" + (output_p - neuron_po) + ")"
+                                );
                             #endif
 
                             // increment pointers
@@ -70,23 +140,18 @@ public class MyBot : IChessBot
                         // apply activation function
                         *output_p = Math.Tanh(*output_p);
 
-                        // increment/reset pointers
+                        // increment pointers
                         output_p++;
-                        input_p -= layers[l - 1];
                     }
-
-                    // move input pointer to next layer
-                    input_p += layers[l - 1];
                 }
             }
         }
-        Console.WriteLine("Eval: " + neurons[^1]);
-    }
 
-    public unsafe Move Think(Board board, Timer timer)
-    {
-        Move[] moves = board.GetLegalMoves();
+        #if DEBUG_LEVEL_2
+            Console.WriteLine("Eval: " + neurons[^1]);
+        #endif
 
-        return moves[0];
+        // return value of output neuron
+        return neurons[^1];
     }
 }
--- Weight-Encoder/Program.cs
+++ Weight-Encoder/Program.cs
@@ -9,25 +9,29 @@ namespace WeightEncoder
     {
         public unsafe static void Main()
         {
+            // weights to encode
             double[] weights = {
-                -.1, -1.75, -1.5, -1.25,
-                0.25, 0.5, 0.75, 1.0,
-                1.25, 1.5, 1.75, 1.99,
-                -.1, -1.75, -1.5, -1.25,
-                0.25, 0.5, 0.75, 1.0,
-                1.25, 1.5, 1.75, 1.99,
-                -.1, -1.75, -1.5, -1.25,
-                0.25, 0.5, 0.75, 1.0,
-                1.25, 1.5, 1.75, 1.99,
-                -.1, -1.75, -1.5, -1.25,
-                0.25, 0.5, 0.75, 1.0,
-                1.25, 1.5, 1.75, 1.99,
+                0.5, 1.0, 1.5, 0.1,
+                -0.5,-1.0, -1.5, -2.0,
+                1.0, 1.0, 1.0, 1.0,
             };
 
-            // Check that weights are divisible by 12
+            // find the min and max weights
+            double min = weights.Min();
+            double max = weights.Max();
+
+            // find the scaling factor and round to three decimal places
+            double scaling_factor = Math.Round(256 / (max-min), 3);
+
+            // determine the shift value
+            double shift = (max + min) / 2;
+
+            Console.WriteLine("Scaling factor: " + scaling_factor + "\nShift: " + shift);
+
+            // check that weights are divisible by 12
             Debug.Assert(weights.Length % 12 == 0, "Weights need to be divisible by 12");
 
-            // Decimals are 16 bytes so we need to make room for the exta 4 bytes
+            // decimals are 16 bytes so we need to make room for the exta 4 bytes
             sbyte[] weight_bytes = new sbyte[weights.Length * 4 / 3];
 
             decimal[] decimals = new decimal[weights.Length / 12];
@@ -51,17 +55,17 @@ namespace WeightEncoder
                             // Loop over all weights in the group
                             for (int j = 0; j < 12; j++)
                             {
-                                // Convert doubles [-2.0, 2.0] to sbyte [-128, 127]
-                                sbyte weight_byte = (sbyte) Math.Round(*weight_p * 64, 0);
+                                // Map doubles [min, max] to sbyte [-128, 127]
+                                sbyte weight_byte = (sbyte) Math.Clamp(Math.Round((*weight_p - shift) * scaling_factor, 0), -128, 127);
 
                                 // Copy the weight byte into the array
                                 *weight_byte_p = weight_byte;
 
                                 #if VERBOSE
                                 // Print weight conversion
-                                Console.WriteLine("\n\nWeight\t (*64)\t\tsbyte\t(Binary)\n");
+                                Console.WriteLine("\n\nWeight\t (* scaling_factor)\t\tsbyte\t(Binary)\n");
                                 Console.WriteLine(
-                                    *weight_p + "\t " + ("(" + (*weight_p * 64) + ")").PadRight(10) + "->\t" + *weight_byte_p + 
+                                    *weight_p + "\t " + ("(" + (*weight_p * scaling_factor) + ")").PadRight(10) + "->\t" + *weight_byte_p + 
                                     "\t(" + Convert.ToString((byte)*weight_byte_p, 2).PadLeft(8, '0') + ")"
                                 );
                                 #endif
